35c960f1ac8dbf060356602101741900aac977bb
Michael K <mk024@pro.hsd1.il.comcast.net>
2016-06-17 16:21:59 -0500

    Initial Commit

---
+++ UnitConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitConverter
+//
+//  Created by Michael K on 6/17/16.
+//  Copyright © 2016 Waxy Watermelon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnitConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnitConverter
+//
+//  Created by Michael K on 6/17/16.
+//  Copyright © 2016 Waxy Watermelon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
