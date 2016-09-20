008055a1cceba033ef6b1ea920528c6a3b9b52ca
Michael Kathan <mk024@comcast.net>
2016-09-20 13:19:14 -0500

    UnitConverter
    Length conversion app for iOS built with Objective C

--- UnitConverter/ViewController.m
+++ UnitConverter/ViewController.m
@@ -8,11 +8,57 @@
 
 #import "ViewController.h"
 
+double convertFeetToMeters(double feetValue){
+    double metersValue;
+    metersValue = feetValue / 3.2808398950131;
+    return metersValue;
+}
+
+double convertFeetToYards(double feetValue){
+    double yardsValue;
+    yardsValue = feetValue / 3.0;
+    return yardsValue;
+}
+
+double convertFeetToMiles(double feetValue){
+    double milesValue;
+    milesValue = feetValue / 5280.0;
+    return milesValue;
+}
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentController;
+
+@property (weak, nonatomic) IBOutlet UILabel *outputField;
+
 
 @end
 
 @implementation ViewController
+
+- (IBAction)updateButton:(id)sender {
+    NSMutableString *buf = [NSMutableString new];
+    
+    double userInput = [self.inputField.text doubleValue];
+    
+    if(self.segmentController.selectedSegmentIndex == 0) {
+        double metersValue = convertFeetToMeters(userInput);
+        [buf appendString:[@(metersValue) stringValue]];
+    }
+    else if(self.segmentController.selectedSegmentIndex == 1) {
+        double yardsValue = convertFeetToYards(userInput);
+        [buf appendString: [@(yardsValue) stringValue]];
+    }
+    else{
+        double milesValue = convertFeetToMiles(userInput);
+        [buf appendString: [@(milesValue) stringValue]];
+    }
+    
+    self.outputField.text = buf;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
